914d266d21a78cdb4323aaf649fca7e4a5389be0
arasad1 <asaduzzamanaj07@gmail.com>
2022-09-18 16:27:22 +0600

    backend-basic-structure

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+// const objectID=require('mongodb').ObjectId;
+
+//Please add .env file in .gitignore
+//DB_USER
+//DB_PASSWORD
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress2").collection("myuser");
+
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World from Example app");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
